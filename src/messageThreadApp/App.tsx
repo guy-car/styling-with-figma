9ac44b3ed5c17603fa22ed7e7bdf9cc60d202273
guy-car <guil.crn@gmail.com>
2025-06-03 13:51:04 -0400

    minor organization changes

--- src/messageThreadApp/App.tsx
+++ src/messageThreadApp/App.tsx
@@ -4,9 +4,7 @@ function App() {
 
   return (
     <div className='font-[Inter] bg-gray-200 flex flex-col'>
-      <h1 className='text-4xl ml-5 pb-5 pt-5'>Task List</h1>
-      <p className='text-sm ml-5'>Sorted by completion</p>
-      <TaskList />
+      <h1 className='text-4xl ml-5 pb-5 pt-5'>Message Thread</h1>
     </div>
   )
 }
